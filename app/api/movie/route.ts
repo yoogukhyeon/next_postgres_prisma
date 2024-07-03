53f20124c1d3ff0ac91e712e80e2e03787e442a6
yoogukhyeon <rnrgus5897@gmail.com>
2024-07-03 17:41:35 +0900

    next.js postgres prisma setting

---
+++ app/api/movie/route.ts
@@ -0,0 +1,10 @@
+import prisma from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+export async function GET(request: Request) {
+	try {
+		const data = await prisma.user.findMany(); // user의 모든 결과 가져오기
+		return Response.json({ data }, { status: 200 });
+	} catch (e) {
+		return NextResponse.json({ message: 'fail' }, { status: 500 });
+	}
+}
